ccf1b8b5065be2fa82f7dad3399820c1a56d8435
RedBird <35497481+redbirdjs@users.noreply.github.com>
2022-09-10 08:15:24 +0200

    Added the basic bot files

---
+++ index.js
@@ -0,0 +1,18 @@
+const { Client, IntentsBitField } = require('discord.js');
+
+const TOKEN = process.env.TOKEN || null;
+
+const intents = [IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMembers, IntentsBitField.Flags.DirectMessages, IntentsBitField.Flags.GuildPresences];
+const client = new Client({ intents: intents });
+
+client.on('ready', () => console.log('The robot is ready!'));
+
+client.on('messageCreate', function(msg) {
+  if (msg.author.bot) return;
+
+  if (msg.content.startsWith('ping')) {
+    msg.reply('Pong!');
+  }
+});
+
+if (TOKEN) client.login(TOKEN);
